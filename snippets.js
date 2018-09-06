059e6f0e5d1c38f64d0622a50da9f12d89b4b02e
Brecht Vermeire <brecht@Brechts-MacBook-Pro.local>
2018-09-06 12:36:02 +0100

    styling

--- snippets.js
+++ snippets.js
@@ -1,4 +1,5 @@
 createWebsite("1aF7Tn6uHd3iUC9ST2O23LiPFK3MpK3Xv3tKR2f1E7vI")
+
 function createWebsite(id) {
     var URL = "https://spreadsheets.google.com/feeds/list/" + id + "/od6/public/values?alt=json"
     $.getJSON(URL, function(data) {
@@ -18,7 +19,7 @@ function createWebsite(id) {
                 "tags": result.gsx$tags.$t
             }
             tags.push(snippet.tags)
-            var template = `<li id="` + snippet.title_id + `" class="snippet-li">
+            var template = `<li id="` + snippet.title_id + `" class="snippet-li ` + snippet.tags + `">
                             <a class="snippet-title" href="#` + snippet.title_id + `">` + snippet.title + `</a>
                             <span class="snippet-tags float-right">` + snippet.tags + `</span>
                             <p class="snippet-content">` + snippet.content + `</p>
@@ -26,5 +27,23 @@ function createWebsite(id) {
                         </li>`
             $(".timeline").append(template).hide().slideDown(200);
         });
+        tags = tags.filter(uniqueTags)
+
+        $.each(tags, function(i, v) {
+            $(".page-tags").append(`<button class='snippet-tag-link' onclick='filterOnTag("` + v + `")'>` + v + `</button>`)
+        })
     })
+}
+
+function filterOnTag(tag) {
+    if(tag == "all") {
+        $(".snippet-li").show()
+    } else {
+    $(".snippet-li").hide()
+    $("." + tag).show()
+    }
+}
+
+function uniqueTags(value, index, self) {
+    return self.indexOf(value) === index;
 }
